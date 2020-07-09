7f423fbf50cc8dbb031418588cbf60d00b551df7
pri8ya <68066400+pri8ya@users.noreply.github.com>
2020-07-09 20:02:57 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,11 @@
+#input
+list1=[12,-7,5,64,-14]
+positivenumbers=[num for num in list1 if num>=0]
+print(positivenumbers)
+
+
+#input2
+list2=[12,14,-95,3]
+positivenumbers=[num for num in list2 if num>=0]
+print(positivenumbers)
+      
